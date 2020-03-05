1f7272c7d04650fa94f36c8951edb09bf7987aff
Caroline Zaini <zainicaroline@gmail.com>
2020-03-05 13:46:00 +0100

    Add connexion's route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ var SHA256 = require("crypto-js/sha256");
 // - Import du module pour encoder en base 64 (64 caractère) :
 var encBase64 = require("crypto-js/enc-base64");
 
-
 var usersModel = require('../models/user')
 
 
@@ -24,7 +23,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-
+/* GET inscription page. */
   router.post('/inscription', async function(req, res, next) {
     console.log("Hello Back")
     console.log('req.body.firstNameFromFront', req.body.firstNameFromFront)
@@ -75,6 +74,52 @@ router.get('/', function(req, res, next) {
     res.json({result, saveUser, error, token})
     
     });
+
+
+
+
+  
+/* GET connexion page. */
+router.get('/connexion', async function(req, res, next) {
+
+  var result = false
+  var user = null
+  var error = []
+  var token = null
+
+  if(req.body.emailFromFront == ''
+  || req.body.passwordFromFront == ''
+  ){
+    error.push('champs vides')
+  }
+
+
+  if (error.length == 0) {
+    const user = await usersModel.findOne({
+      email: req.body.emailFromFront,
+    })
+  }
+
+  if (user) {
+    const passwordEncrypt = SHA256(req.body.passwordFromFront + user.salt).toString(encBase64)
+  
+    if (passwordEncrypt == user.password) {
+      result = true
+      token = user.token 
+    } else {
+      result = false
+      error.push('mot de passe incorrect')
+    }
+  
+  } else {
+    error.push('email incorrect')
+  }
+
+  res.json({result, user, error, token})
+
+
+});
+
 
 
 module.exports = router;
