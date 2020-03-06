56222141f1c4e6a1b376b3f17aac32e4137b9392
Caroline Zaini <zainicaroline@gmail.com>
2020-03-06 13:49:09 +0100

    work on 'monPaiment'

--- routes/index.js
+++ routes/index.js
@@ -10,30 +10,26 @@ var SHA256 = require("crypto-js/sha256");
 // - Import du module pour encoder en base 64 (64 caractère) :
 var encBase64 = require("crypto-js/enc-base64");
 
+
+
 var usersModel = require('../models/user')
 
 
 
 
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
 /* GET inscription page. */
   router.post('/inscription', async function(req, res, next) {
-    console.log("Hello Back")
+
+    console.log("Hello Back Inscription")
     console.log('req.body.firstNameFromFront', req.body.firstNameFromFront)
 
     var result = false;
-    var user = null;
+    var saveUser = null
     var error = [];
     var token = null;
     var salt = uid2(32);
-
+   
     const emailExist = await usersModel.findOne({
       email: req.body.emailFromFront
     });
@@ -48,74 +44,95 @@ router.get('/', function(req, res, next) {
   || req.body.phoneFromFront == ''
   || req.body.passwordFromFront == ''
   ){
-    error.push('champs vides')
+    error.push('Un des champs est vide')
   }
 
   if(error.length == 0) {
+    
+    console.log('1. req.body.email_inscription :', req.body.email_inscription);
+    console.log('2. password_inscription:', req.body.password_inscription);
+
     var newUser = new usersModel ({
       firstName: req.body.firstNameFromFront,
       lastName: req.body.lastNameFromFront,
-      email: req.body.emailFromFront,
+      email: req.body.email_inscription,
       phone: req.body.phoneFromFront,
+      password: SHA256(req.body.password_inscription+salt).toString(encBase64), 
+      token: uid2(32),
       salt: salt,
-      password: SHA256(req.body.passwordFromFront + salt).toString(encBase64),
-      token: uid2(32)
     })
+    
 
     var saveUser = await newUser.save()
+
+    console.log('3.bdd email inscription :', newUser.email_inscription);
+    console.log('4.bdd password inscription', newUser.password_inscription);
+    console.log('5. bdd salt inscription :', newUser.salt);
 
     if(saveUser){
       result = true
       token = saveUser.token
     }
+   
+  } 
 
-  }
+   res.json({result, saveUser, error, token})
 
-    res.json({result, saveUser, error, token})
-    
-    });
+  });
 
 
 
 
   
 /* GET connexion page. */
-router.get('/connexion', async function(req, res, next) {
+router.post('/connexion', async function(req, res, next) {
+
+  console.log('Hello Back connexion', req.body.email_connexion);
 
   var result = false
   var user = null
   var error = []
   var token = null
+  var salt = uid2(32);
 
-  if(req.body.emailFromFront == ''
-  || req.body.passwordFromFront == ''
+  if(req.body.email_connexion == ''
+  || req.body.password_connexion == ''
   ){
-    error.push('champs vides')
+    error.push('Un champs est vide')
+    console.log('error :', error);
   }
 
 
-  if (error.length == 0) {
-    const user = await usersModel.findOne({
-      email: req.body.emailFromFront,
-    })
+  if(error.length == 0){
+      user = await usersModel.findOne({email: req.body.email_connexion})
   }
 
-  if (user) {
-    const passwordEncrypt = SHA256(req.body.passwordFromFront + user.salt).toString(encBase64)
+  console.log('user :', user);
   
-    if (passwordEncrypt == user.password) {
+  if (user) {
+    
+
+    var passwordEncrypt =  SHA256(req.body.password_connexion + user.salt).toString(encBase64) 
+
+    console.log('salt bdd :', user.salt);
+    console.log('passwordEncrypt :', passwordEncrypt);
+    console.log('password bdd :', user.password);
+
+    if (passwordEncrypt === user.password) {
       result = true
       token = user.token 
+      res.json({result, user, error, token})
     } else {
       result = false
-      error.push('mot de passe incorrect')
+      error.push('Mot de passe incorrect')
+      res.json({result, error, token})
     }
   
   } else {
     error.push('email incorrect')
   }
 
-  res.json({result, user, error, token})
+  
 
 
 });
